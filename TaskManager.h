04687acf3e5d371e932278c060691daf7ad67fcf
0xvasq <jorge.vasquez007@mymdc.net>
2024-05-03 16:39:34 -0400

    Start and create TaskManager ckass

--- TaskManager.h
+++ TaskManager.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "Task.h"
+#include <vector>
+
+class TaskManager {
+	std::vector<Task> tasks;
+
+public:
+
+	void addTask(int, std::string, std::string, int, std::string);
+};
+
+void TaskManager::addTask(int taskID, std::string taskTitle, std::string taskDescr, int taskPrty, std::string taskStatus) {
+	Task newTask(taskID, taskTitle, taskDescr, taskPrty, taskStatus);
+	tasks.push_back(newTask);
+}
+
+
